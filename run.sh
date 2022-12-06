202c86fc08fd4a5db6040db4538b44c45c4c5c3d
Harshad Bhandwaldar <89623135+hb0313@users.noreply.github.com>
2022-12-06 02:35:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
 echo 'running docker'
+echo "Building docker image using shell!!!"
 docker build -t app1 .
-docker rm image app1
+echo "Build successfull, cleaning up..."
+docker image rm app1
